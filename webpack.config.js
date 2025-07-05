4effe8624193ed042ea3f9a9d95b524b61fb87c3
Services <89109947+rfoldhzi@users.noreply.github.com>
2025-07-05 15:18:51 -0500

    Fix Build Script

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+// webpack.config.js
+module.exports = {
+    // ... other webpack configurations
+    output: {
+        // ... other output options
+        chunkFormat: 'commonjs', // Or 'commonjs' for Node.js
+    },
+};
